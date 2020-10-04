138e5ba2dab702bf389ac2cc79f5bc23ce294f8c
Rohit Raj Jalheria <rjalheira@gmail.com>
2020-10-04 23:09:49 +0530

    Update and rename missing_data.py to 01_DataPreprocessing/missing_data.py

--- 01_DataPreprocessing/missing_data.py
+++ 01_DataPreprocessing/missing_data.py
@@ -1,5 +1,5 @@
 # Missing data
-#clen up missing data (NaN) using the mean
+#clean up missing data (NaN) using the mean
 
 # Importing the libraries
 import numpy as np
@@ -9,7 +9,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, -1].values
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
